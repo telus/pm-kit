2d6262347baf115702f0109f2e15479361e97059
Sonny Zhang <szhang@nascentdigital.com>
2019-11-25 17:03:50 -0500

    chore(storybook): added storysource addon

--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -1,10 +1,19 @@
-const path = require('path')
 //.storybook/webpack.config.js
 module.exports = async webpack => {
   const { config } = webpack
+
+  // for @storybook/addon-storysource
+  config.module.rules.unshift({
+    test: /\.stories\.jsx?$/,
+    loaders: [require.resolve('@storybook/source-loader')],
+    enforce: 'pre',
+  })
+
+  // for ignoring packages' node_modules
   const jsRule = config.module.rules.find(rule => {
     return String(rule.test) === String(/\.(mjs|jsx?)$/)
   })
   jsRule.exclude = /node_modules/
+
   return config
 }
