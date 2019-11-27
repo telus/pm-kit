82172cf2592e11826b8c730347160c0ff49f88d3
Sonny Zhang <szhang@nascentdigital.com>
2019-11-27 14:51:28 -0500

    chore(storybook): added knobs addon for storybook

--- packages/Button/Button.stories.js
+++ packages/Button/Button.stories.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
+import { withKnobs, text, select, boolean } from '@storybook/addon-knobs'
 import Button from './Button.jsx'
 
 export default {
   title: 'Button',
+  decorators: [withKnobs],
 }
 
 export const Primary = () => <Button onClick={action('clicked')}>Primary Button</Button>
@@ -21,7 +23,21 @@ export const Inverted = () => (
 )
 
 export const Disabled = () => (
-  <Button variant="disabled" disabled onClick={action('clicked')}>
+  <Button disabled onClick={action('clicked')}>
     Disabled Button
   </Button>
 )
+
+export const Playground = () => (
+  <Button
+    variant={select('Variant', ['primary', 'secondary', 'inverted'])}
+    disabled={boolean('Disabled', false)}
+    onClick={action('clicked')}
+  >
+    {text('Label', 'Testing')}
+  </Button>
+)
+
+Playground.story = {
+  name: 'playground',
+}
