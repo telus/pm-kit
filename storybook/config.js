ca7a0362330ae0af3186238305220a9c36b4f570
Sonny Zhang <szhang@nascentdigital.com>
2019-11-27 22:33:49 -0500

    chore(storybook): added accessibility testing

--- storybook/config.js
+++ storybook/config.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { configure, addDecorator } from '@storybook/react'
+import { withA11y } from '@storybook/addon-a11y'
 import GlobalStyles from './GlobalStyles'
 
+addDecorator(withA11y)
 addDecorator(story => (
   <>
     <GlobalStyles />
