ae388f4e9c6b4ac2181b4194fd71b69c0dd530e2
pbrahmbhatt3 <pbrahmbhatt@nascentdigital.com>
2019-12-04 14:56:53 -0500

    chore(checkbox): added a story to display error

--- packages/Checkbox/Checkbox.stories.js
+++ packages/Checkbox/Checkbox.stories.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import Checkbox from './Checkbox'
+import { withKnobs, select } from '@storybook/addon-knobs'
 
 export default {
   title: 'Design System|Checkbox',
   component: Checkbox,
+  decorators: [withKnobs],
 }
 
 export const Default = () => {
@@ -13,5 +15,62 @@ export const Default = () => {
     setChecked(!checked)
     return checked
   }
-  return <Checkbox label="Testing" name="Test" value={true} onChange={updateCheck} checked={checked} />
+  return (
+    <Checkbox
+      label="Testing"
+      name="Test"
+      value={true}
+      onChange={updateCheck}
+      checked={checked}
+      feedback="success"
+      error="This field is required"
+    />
+  )
+}
+
+export const CheckboxWithError = () => {
+  const [checked, setChecked] = useState(false)
+
+  const updateCheck = () => {
+    setChecked(!checked)
+    return checked
+  }
+  return (
+    <Checkbox
+      label="Testing"
+      name="Test"
+      value={true}
+      onChange={updateCheck}
+      checked={checked}
+      //   feedback={checked ? 'success' : 'error'}
+      feedback="error"
+      error="This field is required"
+    />
+  )
+}
+
+export const Playground = () => {
+  const [checked, setChecked] = useState(false)
+
+  const updateCheck = () => {
+    setChecked(!checked)
+    return checked
+  }
+
+  return (
+    <Checkbox
+      label="Testing"
+      name="Test"
+      value={true}
+      onChange={updateCheck}
+      checked={checked}
+      feedback={select('feedback', ['error', 'success'])}
+      error="This field is required"
+    />
+  )
+}
+
+Playground.story = {
+  name: 'playground',
+  parameters: { docs: { page: null, disable: true } },
 }
