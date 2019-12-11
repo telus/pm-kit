32d3ab5bdcc25e81349eb9ed8af3c3407c77fcd1
Sonny Zhang <szhang@nascentdigital.com>
2019-12-10 21:51:45 -0500

    chore(config): removed emotion source maps in builds, updated npmignore

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = function(api) {
   return {
     presets: ['@babel/preset-env', '@babel/preset-react', '@emotion/babel-preset-css-prop'],
     plugins: [
+      ['emotion', { sourceMap: false }],
       ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
       '@babel/plugin-transform-runtime',
       '@babel/plugin-proposal-class-properties',
@@ -12,6 +13,7 @@ module.exports = function(api) {
       test: {
         presets: ['@babel/preset-env', '@babel/preset-react', '@emotion/babel-preset-css-prop'],
         plugins: [
+          ['emotion', { sourceMap: false }],
           ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
           '@babel/plugin-transform-runtime',
           '@babel/plugin-proposal-class-properties',
