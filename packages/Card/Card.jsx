a351b67fb459244cbaebf9257df651ac28e0d1bb
Kaeto <kndu@nascentdigital.com>
2020-01-23 13:13:39 -0500

    chore(card): add card files and folders

---
+++ packages/Card/Card.jsx
@@ -0,0 +1,239 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { css } from '@emotion/core'
+import { motion, AnimatePresence } from 'framer-motion'
+import { offWhite, parkGreen, lilyGreen } from '@pm-kit/colours'
+import { weight } from '@pm-kit/typography'
+import downArrow from '../../shared/svg/arrow-show.svg'
+import upArrow from '../../shared/svg/arrow-hide.svg'
+import checkmark from '../../shared/png/Checkmark/verified@3x.png'
+import oval from '../../shared/png/EmptyOval/oval@3x.png'
+import Paragraph from '@pm-kit/paragraph'
+
+const card = css`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: ${offWhite};
+  border-width: 3px;
+  border-radius: 12px;
+  border-style: solid;
+  overflow: hidden;
+  position: relative;
+  border-color: ${parkGreen};
+  flex-basis: '40%';
+  flex-grow: '1';
+  margin-bottom: '32px';
+  &:hover {
+    box-shadow: 0 2px 18px 0 rgba(0, 0, 0, 0.25);
+  }
+`
+
+const selectedCard = css`
+  background-color: ${lilyGreen};
+  box-shadow: 0 2px 18px 0 rgba(0, 0, 0, 0.25);
+`
+
+const titleStyle = css`
+  color: ${parkGreen};
+  font-weight: ${weight.bold};
+  padding-left: 8px;
+  width: 100%;
+`
+
+const paddingLeft = css`
+  padding-left: 8px;
+`
+
+const line = css`
+  border-style: solid;
+  border-color: ${parkGreen};
+  border-bottom-width: 1px;
+  margin-left: 35px;
+  margin-right: 35px;
+`
+
+const detailsBar = css`
+  display: flex;
+  flex-direction: row;
+  font-weight: ${weight.bold};
+  justify-content: space-between;
+  text-decoration: underline;
+  padding: 16px 23px 23px 23px;
+  cursor: pointer;
+`
+
+const cardRowContainer = css`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  margin-bottom: 24px;
+  margin-top: 8px;
+`
+
+const selectOptionContainer = css`
+  padding: 23px 23px 0px 23px;
+  cursor: pointer;
+`
+
+const cardDetails = css`
+  color: ${parkGreen};
+  padding: 0px 23px 0px 23px;
+  overflow: hidden;
+`
+
+const detailSection = css`
+  padding-top: 16px;
+  padding-bottom: 24px;
+  display: flex;
+  flex-direction: column;
+  & > ul {
+    list-style: disc;
+    list-style-position: inside;
+  }
+  & > ul > li > span {
+    margin-left: -12px;
+  }
+`
+
+const selectOption = css`
+  display: flex;
+  flex-direction: row-reverse;
+  font-weight: ${weight.bold};
+`
+
+const planSelected = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const cardSelectedText = css`
+  padding-right: 5px;
+`
+const cardNotSelectedText = css`
+  text-decoration: underline;
+  padding-right: 8px;
+`
+const cardContainerDetails = css`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+`
+const placeholderContainer = css`
+  max-width: 20%;
+  display: flex;
+`
+
+const planVariant = {
+  hidden: { opacity: 0, y: -10 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { y: { type: 'spring', damping: 100, velocity: 500 }, opacity: { duration: 0.8 } },
+  },
+}
+
+const Card = ({ placeholder, title, subtitle, children, onClick, isSelected, selectable }) => {
+  const [openCard, setOpenCard] = useState(false)
+
+  const styles = [card]
+
+  if (isSelected) {
+    styles.push(selectedCard)
+  }
+
+  const onCardClick = () => {
+    onClick()
+  }
+
+  const toggleOpenCard = () => {
+    setOpenCard(!openCard)
+  }
+
+  let arrowImage = downArrow
+
+  if (openCard) {
+    arrowImage = upArrow
+  } else {
+    arrowImage = downArrow
+  }
+
+  return (
+    <motion.div css={styles} variants={planVariant}>
+      <div css={cardContainerDetails}>
+        <div css={selectOptionContainer} onClick={selectable ? onCardClick : undefined}>
+          {selectable && (
+            <div css={selectOption}>
+              {isSelected && (
+                <div css={planSelected}>
+                  <p css={cardSelectedText}>{selectable.selectedText}</p>
+                  <img src={checkmark} height="18px" width="18px" alt="selected" />
+                </div>
+              )}
+              {!isSelected && (
+                <div css={planSelected}>
+                  <p css={cardNotSelectedText}>{selectable.unSelectedText}</p>
+                  <img src={oval} height="18px" width="18px" alt="unselected" />
+                </div>
+              )}
+            </div>
+          )}
+
+          <div css={cardRowContainer}>
+            <div css={placeholderContainer}>{placeholder && placeholder}</div>
+            <div>
+              <p css={titleStyle}>{title}</p>
+              <p css={paddingLeft}>{subtitle}</p>
+            </div>
+          </div>
+        </div>
+        {children && (
+          <>
+            <div css={line}></div>
+            <AnimatePresence>
+              {openCard && (
+                <motion.div
+                  css={cardDetails}
+                  onClick={toggleOpenCard}
+                  initial={{ height: 0 }}
+                  animate={{ height: 'auto' }}
+                  exit={{ height: '0px' }}
+                >
+                  <div css={detailSection}>{children}</div>
+                </motion.div>
+              )}
+            </AnimatePresence>
+
+            <div css={detailsBar} onClick={toggleOpenCard}>
+              {openCard && <Paragraph>Collapse</Paragraph>}
+              {!openCard && <Paragraph>Details</Paragraph>}
+              <img src={arrowImage} alt="arrow" />
+            </div>
+          </>
+        )}
+      </div>
+    </motion.div>
+  )
+}
+
+Card.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  children: PropTypes.node,
+  onClick: PropTypes.func,
+  isSelected: PropTypes.bool,
+  selectable: PropTypes.object,
+}
+
+Card.defaultProps = {
+  title: undefined,
+  subtitle: undefined,
+  children: undefined,
+  onClick: undefined,
+  isSelected: undefined,
+  selectable: undefined,
+}
+
+export default Card
