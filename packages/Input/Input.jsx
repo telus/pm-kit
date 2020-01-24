d3742c32fccabb85636a13b1b72cd5a7c09f15cb
pbrahmbhatt3 <pbrahmbhatt@nascentdigital.com>
2020-01-24 09:31:14 -0500

    chore(input): removed emotion/styled import

--- packages/Input/Input.jsx
+++ packages/Input/Input.jsx
@@ -1,7 +1,6 @@
 import React, { forwardRef } from 'react'
 import PropTypes from 'prop-types'
 import { css } from '@emotion/core'
-import styled from '@emotion/styled'
 import { red, parkGreen, greyBlue } from '@pm-kit/colours'
 import generateId from '../../shared/utils/generateId/generateId.js'
 import FeedbackIcon from '@pm-kit/feedback-icon'
